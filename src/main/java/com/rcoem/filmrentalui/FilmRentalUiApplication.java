3e498dc085dd77d552c533eca3013a8fdecf960f
Ekansh10 <ekanshmahajan1010@gmail.com>
2026-03-31 11:07:40 +0530

    Initial Commit

---
+++ src/main/java/com/rcoem/filmrentalui/FilmRentalUiApplication.java
@@ -0,0 +1,13 @@
+package com.rcoem.filmrentalui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmRentalUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmRentalUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rcoem/filmrentalui/FilmRentalUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcoem.filmrentalui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmRentalUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
