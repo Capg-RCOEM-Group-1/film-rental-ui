01c6e419440b47a185d174001c1d6c8ce536b0dd
Ekansh10 <ekanshmahajan1010@gmail.com>
2026-04-01 14:43:01 +0530

    Added Customer Supporting DTOs, HTML files as well :-)

--- src/main/java/com/rcoem/filmrentalui/dto/CustomerStoreDTO.java
+++ src/main/java/com/rcoem/filmrentalui/dto/CustomerStoreDTO.java
@@ -1,10 +1,41 @@
 package com.rcoem.filmrentalui.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CustomerStoreDTO {
-    private Byte storeId;
+    private Short storeId; // Still needed for your Java logic
+    private String address;
+    private String city;
+
+    // Getters and Setters
+    public Short getStoreId() { return storeId; }
+    public void setStoreId(Short storeId) { this.storeId = storeId; }
+
+    @com.fasterxml.jackson.annotation.JsonProperty("_links")
+    public void unpackId(java.util.Map<String, Object> links) {
+        try {
+            java.util.Map<String, String> self = (java.util.Map<String, String>) links.get("store");
+            if (self == null) self = (java.util.Map<String, String>) links.get("self");
+            if (self != null) {
+                String href = self.get("href").replaceAll("\\{.*\\}", "");
+                String idStr = href.substring(href.lastIndexOf('/') + 1);
+                this.storeId = Short.parseShort(idStr);
+            }
+        } catch (Exception e) {}
+    }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public String getFullDisplay() {
+        return address != null ? (city != null ? address + ", " + city : address) : "";
+    }
 }
